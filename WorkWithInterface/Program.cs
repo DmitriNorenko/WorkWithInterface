e2b12f4c23cecf90eb552907f5da9bfd06e91c74
Name <email@exaple.ru>
2023-06-29 08:10:48 +0300

    10.1.4

--- WorkWithInterface/Program.cs
+++ WorkWithInterface/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WorkWithInterface.Program;
 
 namespace WorkWithInterface
 {
@@ -10,6 +11,35 @@ namespace WorkWithInterface
     {
         static void Main(string[] args)
         {
+        }
+        public interface IManager
+        {
+            void Create();
+            void Read();
+            void Update();
+            void Delete();
+        }
+        public class Manager : IManager
+        {
+            public void Create()
+            {
+
+            }
+
+            public void Read()
+            {
+
+            }
+
+            public void Update()
+            {
+
+            }
+
+            public void Delete()
+            {
+
+            }
         }
     }
 }
